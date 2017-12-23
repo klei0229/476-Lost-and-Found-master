f3961be32887a7cea0d0c94b70bd1c13d384ff17
YuHaoLiu <liuyuhao717@gmail.com>
2017-12-22 23:45:15 -0500

    tests of existance of files in templates

--- test_existance.py
+++ test_existance.py
@@ -65,3 +65,76 @@ class Test_exstance (unittest.TestCase):
                           'improv_app',
                           'test_routes.py'))
         self.assertTrue(file_exits)
+
+    def test_check_improve_app_templates_aboutus_html_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'aboutus.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_chatroom_html_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'chatroom.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_chatroom_video_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'chatroomvideo.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_create_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'create_page.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_index_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'index.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_layout_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'layout.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_login_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'login.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_search_page_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'search_page.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_search_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'search.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_session_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'session.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_signup_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'signup.html'))
+        self.assertTrue(file_exits)
+    def test_check_improve_app_templates_yuhao_test_file(self):
+        file_exits = op.exists(op.join(self.dir,
+                          'improv_app',
+                          'templates',
+                          'yuhao_test.html'))
+        self.assertTrue(file_exits)
